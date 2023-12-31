ddea651b3f1445a03bc10cb94c89c715eb93a2e1
Minh Hiếu <99780315+CamenRyder@users.noreply.github.com>
2023-12-31 19:55:37 +0700

    add permission
    add permision : use camera, read, write storage

---
+++ ping_meme/lib/services/imagePicker/image_picker.dart
@@ -0,0 +1,13 @@
+import 'package:image_picker/image_picker.dart';
+
+// class ImagePicker {
+//   ImageSource source ;  
+
+
+//   ImagePicker({required this.source}); 
+
+//   pick({onClick}) async {
+//     final picker = ImagePicker(source: source); 
+//     final pickedFile = await picker.source.pi
+//   }
+// }
